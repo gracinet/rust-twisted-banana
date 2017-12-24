b2955c0a8a7ae43d6e92b7cd43730a3550539be0
Georges Racinet <georges@racinet.fr>
2017-12-24 18:46:11 +0100

    Banana lists

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,7 @@ pub enum Element {
     Integer(i32), // split into Integer (0x81) and Negative Integer (0x83)
     String(Vec<u8>), // 0x82
     Float(f64), // 0x84
+    List(Vec<Element>), // 0x80
 }
 
 /// Base128 used for length
@@ -20,7 +21,7 @@ pub enum DecodeError {
     Empty,
     UnknownType,
     OverFlow(Vec<u8>),
-    TooShort(usize, usize), // (expected, actual)
+    TooShort(usize, usize), // contains (expected, actual)
     Invalid(String),
 }
 
@@ -83,7 +84,7 @@ impl Element {
     }
 
     fn dec_string(length_bytes: &[u8], full_msg: &[u8]) -> Result<Vec<u8>, DecodeError> {
-        let l = Self::dec_posint(length_bytes)? as usize;
+        let l = Self::dec_posint(length_bytes)? as usize; // TODO big len
         let start = length_bytes.len() + 1;
         let end = start + l;
         if end > full_msg.len() {
@@ -122,17 +123,68 @@ impl Element {
     }
 
     /// Decode an element, incuding length marker,
-    /// and return an owned Banana object
-    /// TODO return remainder, maybe consume incoming bytes, then to get 0-copy
-    pub fn from_bytes(bytes: &[u8]) -> Result<Self, DecodeError> {
+    /// and return an owned Banana object, together with remaning unused bytes
+    /// maybe consume incoming bytes, to get a 0-copy ?
+    /// Would be nice also to allow for partial messages, allowing to stream very long
+    /// communications without having to wait for completion and represent the full content in
+    /// RAM. Check what applications (e.g., buildbot) actually do for big communications.
+    /// stream within the protocol or outside of it ?
+    pub fn from_bytes_rem<'a>(bytes: &'a [u8]) -> Result<(Self, &'a [u8]), DecodeError> {
         let (length_bytes, elt_type) = Self::length_type(bytes).unwrap();
         match elt_type {
-            0x81 => Ok(Element::Integer(Self::dec_posint(length_bytes)? as i32)),
-            0x83 => Ok(Element::Integer(Self::dec_negint(length_bytes)? as i32)),
-            0x82 => Ok(Element::String(Self::dec_string(length_bytes, bytes)?)),
-            0x84 => Ok(Element::Float(Self::dec_float(length_bytes, bytes)?)),
+            0x81 => {
+                Ok((
+                    Element::Integer(Self::dec_posint(length_bytes)? as i32),
+                    &bytes[length_bytes.len() + 1..],
+                ))
+            }
+            0x83 => {
+                Ok((
+                    Element::Integer(Self::dec_negint(length_bytes)? as i32),
+                    &bytes[length_bytes.len() + 1..],
+                ))
+            }
+            0x82 => {
+                let st = Self::dec_string(length_bytes, bytes)?;
+                let stl = st.len();
+                Ok((
+                    Element::String(st),
+                    &bytes[length_bytes.len() + 1 + stl..],
+                ))
+            }
+            0x80 => Self::dec_list(length_bytes, bytes),
+            0x84 => {
+                Ok((
+                    Element::Float(Self::dec_float(length_bytes, bytes)?),
+                    &bytes[9..],
+                ))
+            }
             _ => Err(DecodeError::UnknownType),
         }
+    }
+
+    fn dec_list<'a>(
+        length_bytes: &[u8],
+        full_msg: &'a [u8],
+    ) -> Result<(Self, &'a [u8]), DecodeError> {
+        if length_bytes.len() == 0 {
+            return Err(DecodeError::Invalid("List without a length".into()));
+        }
+        let list_len = Self::dec_posint(length_bytes)? as usize; // TODO big len
+        let mut resv: Vec<Self> = Vec::with_capacity(list_len);
+        let mut rem = &full_msg[length_bytes.len() + 1..];
+        for _i in 0..list_len {
+            let item_rem = Self::from_bytes_rem(rem)?;
+            resv.push(item_rem.0);
+            rem = item_rem.1;
+        }
+        Ok((Element::List(resv), rem))
+    }
+
+    #[inline]
+    /// Decode an element, including length/type preamble, and ignore the remainder
+    pub fn from_bytes(bytes: &[u8]) -> Result<Self, DecodeError> {
+        Ok(Self::from_bytes_rem(bytes)?.0)
     }
 }
 
@@ -201,6 +253,20 @@ mod tests {
     }
 
     #[test]
+    fn decode_list() {
+        let bytes: &[u8] = &[0x02, 0x80, 0x02, 0x81, 0x03, 0x83];
+        assert_eq!(
+            Element::from_bytes(&bytes).unwrap(),
+            Element::List(vec![Element::Integer(2), Element::Integer(-3)])
+        );
+        let bytes: &[u8] = &[0x80];
+        assert_eq!(
+            Element::from_bytes(&bytes),
+            Err(DecodeError::Invalid("List without a length".into()))
+        );
+    }
+
+    #[test]
     /// Examples from the banana spec
     fn spec_examples() {
         // integer
@@ -221,6 +287,38 @@ mod tests {
             Element::String(String::from("hello").into_bytes())
         );
 
+        // lists
+        let bytes: &[u8] = &[0, 0x80];
+        assert_eq!(Element::from_bytes(&bytes).unwrap(), Element::List(vec![]));
+        let bytes: &[u8] = &[2, 0x80, 0x01, 0x81, 0x17, 0x81];
+        assert_eq!(
+            Element::from_bytes(&bytes).unwrap(),
+            Element::List(vec![Element::Integer(1), Element::Integer(23)])
+        );
+        let bytes: &[u8] = &[
+            2,
+            0x80,
+            1,
+            0x81,
+            1,
+            0x80,
+            5,
+            0x82,
+            0x68,
+            0x65,
+            0x6c,
+            0x6c,
+            0x6f,
+        ];
+        assert_eq!(
+            Element::from_bytes(&bytes).unwrap(),
+            Element::List(vec![
+                Element::Integer(1),
+                Element::List(vec![
+                    Element::String(String::from("hello").into_bytes()),
+                ]),
+            ])
+        );
     }
 
     #[test]
